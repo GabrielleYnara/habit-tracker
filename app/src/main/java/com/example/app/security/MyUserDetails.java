151fbdda435de062ae606e8899d4a76eb96e2cfb
Gabrielle Ynara <gabrielleynara@ymail.com>
2023-09-19 18:43:38 -0500

    Add relevant Javadoc string to MyUserDetails

--- app/src/main/java/com/example/app/security/MyUserDetails.java
+++ app/src/main/java/com/example/app/security/MyUserDetails.java
@@ -5,8 +5,18 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.HashSet;
 
+/**
+ * Holds user information for authorization and authentication. <br>
+ * Implements the UserDetails interface from Spring Security.
+ *
+ * @version 1.0.0
+ */
 public class MyUserDetails implements UserDetails {
+    /**
+     * Represents the associated User object.
+     */
     private final User user;
 
     public MyUserDetails(User user) {
@@ -17,38 +27,57 @@ public class MyUserDetails implements UserDetails {
         return user;
     }
 
+    /**
+     * Returns a collection of authorities (permissions) granted to the user.
+     */
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return new HashSet<>();
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return user.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return user.getEmailAddress();
     }
 
+    /**
+     * @return true if Account is not expired,
+     *         false otherwise.
+     */
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
+    /**
+     * @return true if Account is not locked,
+     *         false otherwise.
+     */
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
+    /**
+     * @return true if Credentials are not expired,
+     *         false otherwise.
+     */
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
+    /**
+     * @return true if Account is enabled,
+     *         false otherwise.
+     */
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
