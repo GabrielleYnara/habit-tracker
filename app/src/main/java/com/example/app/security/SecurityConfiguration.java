d3d4752aff9a9ec05dd5c46071ac92c306fec9e2
Gabrielle Ynara <gabrielleynara@ymail.com>
2023-09-19 21:17:25 -0500

    Update SecurityConfiguration, add authenticationManager() bean

--- app/src/main/java/com/example/app/security/SecurityConfiguration.java
+++ app/src/main/java/com/example/app/security/SecurityConfiguration.java
@@ -49,5 +49,55 @@ public class SecurityConfiguration {
         return http.build();
     }
 
+    /**
+     * Responsible to configure security settings for the application.<br>
+     * This class contains beans and configuration settings related to authentication and authorization.
+     *
+     * @version 1.1.0
+     */
+    @Configuration
+    @EnableGlobalMethodSecurity(prePostEnabled = true)
+    public class SecurityConfiguration {
+        /**
+         * Creates and returns a BCryptPasswordEncoder object.
+         *
+         * @return BCryptPasswordEncoder
+         */
+        @Bean
+        public BCryptPasswordEncoder passwordEncoder(){
+            return new BCryptPasswordEncoder();
+        }
 
+        /**
+         * Configures and returns a SecurityFilterChain based on the provided HttpSecurity object.
+         *
+         * @param http HttpSecurity object used for configuration.
+         * @return SecurityFilterChain
+         * @throws Exception if an error occurs during configuration.
+         */
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+            //policies on every request:
+            http.authorizeRequests().antMatchers("/auth/users", "/auth/users/login", "/auth/users/register").permitAll() //public end-points
+                    .antMatchers("/h2-console/**").permitAll() //access to database
+                    .anyRequest().authenticated()
+                    .and().sessionManagement()
+//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) //every time will check for jwt token, not session based.
+                    .and().csrf().disable()
+                    .headers().frameOptions().disable();
+            return http.build();
+        }
+
+        /**
+         * Creates and returns an AuthenticationManager based on the provided AuthenticationConfiguration.
+         *
+         * @param authConfig AuthenticationConfiguration object.
+         * @return AuthenticationManager
+         * @throws Exception if an error occurs during creation.
+         */
+        @Bean
+        public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception{
+            return authConfig.getAuthenticationManager();
+        }
+    }
 }
