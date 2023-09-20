033a8ba6e04eaf9eea56e799680de23ded744614
Gabrielle Ynara <gabrielleynara@ymail.com>
2023-09-19 19:26:32 -0500

    Update UserRepository, Add findUserByEmailAddress custom method

--- app/src/main/java/com/example/app/repository/UserRepository.java
+++ app/src/main/java/com/example/app/repository/UserRepository.java
@@ -4,6 +4,19 @@ import com.example.app.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ *  A Spring Data JPA repository for managing User entities.<br>
+ *  It extends JpaRepository, which provides methods for basic CRUD operations.
+ *
+ * @version 1.1.0
+ */
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> { //Entity and primary key data type
+    /**
+     * Checks if a user with given emailAddress exists.
+     * @param emailAddress
+     * @return A User if it finds a match,
+     *         null otherwise.
+     */
+    User findUserByEmailAddress(String emailAddress);
 }
